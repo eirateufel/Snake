bc5f4294ec4ac5fa3c63bde61ee80d4116765e57
eirateufel <sv.tolstyakova@gmail.com>
2018-04-09 13:54:32 +0300

    Font + classes

---
+++ game.py
@@ -0,0 +1,127 @@
+import pygame
+import os
+from random import *
+from queue import Queue
+from collections import deque
+
+from Score import Score
+from Snake import Snake
+
+
+def draw_point():
+    global radius, formsize, squareSize, x_point_pos, y_point_pos
+    pygame.draw.circle(screen, point_color, (x_point_pos, y_point_pos), radius)
+
+def move_point():
+    global formsize, squareSize, x_point_pos, y_point_pos
+    x_point_pos = (randint(squareSize, formSize - squareSize)//squareSize)*squareSize
+    y_point_pos = (randint(squareSize, formSize - squareSize)//squareSize)*squareSize
+
+
+#form
+formSize = 600
+pygame.init()
+screen = pygame.display.set_mode((formSize, formSize))
+
+#square
+squareSize = 20
+
+#point
+radius = 3
+x_point_pos = (randint(squareSize/2, formSize - squareSize/2)//squareSize)*squareSize
+y_point_pos = (randint(squareSize/2, formSize - squareSize/2)//squareSize)*squareSize
+point_color = (255, 165, 0)
+
+#Score
+pygame.font.init()
+#font = pygame.font.SysFont('Comic Sans MS', 30)
+font = pygame.font.Font(os.path.join("res", "fonts", 'manaspc.ttf'), 16)
+score = Score(screen, font)
+
+#Snake
+snake = Snake(screen, 20)
+
+time = 30/15
+x_speed_direction = 1
+y_speed_direction = 0
+
+pygame.init()
+screen = pygame.display.set_mode((formSize, formSize))
+
+clock = pygame.time.Clock()
+
+done = False
+counter = 1
+eaten = False
+button = False
+
+while not done:
+
+    screen.fill((0, 0, 0))
+    button = False
+    eaten = False
+
+    if x_point_pos == (snake.x_pos + squareSize/2) and y_point_pos == (snake.y_pos + squareSize/2):
+        snake.eat_point()
+        
+        counter += 1
+        if counter % 10 == 0:
+            time = time * 1.2
+        move_point()
+        eaten = True
+        score.increase()
+        
+    if not eaten:
+        draw_point()
+
+    for event in pygame.event.get():            
+        if event.type == pygame.QUIT:
+            done = True
+            pygame.quit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT and not button:
+                if snake.get_speed_x() == 0:
+                    snake.set_speed(1, 0)
+                    button = True
+                    continue
+                else:
+                    continue
+            if event.key == pygame.K_LEFT and not button:
+                if snake.get_speed_x() == 0:
+                    snake.set_speed(-1, 0)
+                    button = True
+                    continue
+                else:
+                    continue
+            if event.key == pygame.K_UP and not button:
+                if snake.get_speed_y() == 0:
+                    snake.set_speed(0, -1)
+                    button = True
+                    continue
+                else:
+                    continue
+            if event.key == pygame.K_DOWN and not button:
+                if snake.get_speed_y() == 0:
+                    snake.set_speed(0, 1)
+                    button = True
+                    continue
+                else:
+                    continue
+
+    snake.move()
+    snake.draw()
+
+    score.draw()
+
+    if snake.is_self_intersect():
+        snake.set_speed(0, 0)
+        screen.fill((0, 0, 0))
+        pygame.font.Font(None, 80)
+        text = font.render("GAME OVER!", False, (255, 255, 255))
+        screen_width = screen.get_rect()[2]
+        screen_height = screen.get_rect()[3]
+        screen.blit(text, (screen_width/2 - text.get_width()/2, screen_height/2))
+
+    pygame.display.flip()
+    clock.tick(time)
+    
--- snake.py
+++ snake.py
@@ -1,137 +1,56 @@
 import pygame
-from random import *
-from queue import Queue
 from collections import deque
 
-def draw_body(x_body, y_body):
-    pygame.draw.rect(screen, (0, 0, 255), pygame.Rect(x_body, y_body, squareSize, squareSize))
+class Snake:
+    def __init__(self, screen, square_size):
+        self.screen = screen
+        self.screen_width = self.screen.get_rect()[2]
+        self.screen_height = self.screen.get_rect()[3]
 
-def move():
-    global x_pos, y_pos
+        self.x_pos = screen.get_rect()[2] / 2 - square_size / 2
+        self.y_pos = screen.get_rect()[3] / 2 - square_size / 2
+        self.square_size = square_size
 
-    x_pos += x_speed_direction * squareSize
-    y_pos += y_speed_direction * squareSize
-    x_body.append(x_pos)
-    y_body.append(y_pos)
-    x_body.popleft()
-    y_body.popleft()
-    
-def move_eaten():
-    global x_pos, y_pos
+        self.x_body = deque()
+        self.x_body.append(self.x_pos)
+        self.y_body = deque()
+        self.y_body.append(self.y_pos)
 
-    x_pos += x_speed_direction * squareSize
-    y_pos += y_speed_direction * squareSize
-    x_body.append(x_pos)
-    y_body.append(y_pos)
+        self.x_speed_direction = 0
+        self.y_speed_direction = 1
 
-def set_speed(x, y):
-    global x_speed_direction, y_speed_direction
-    x_speed_direction = x
-    y_speed_direction = y
+        self.counter = 1
 
-def draw_point():
-    global radius, formsize, squareSize, x_point_pos, y_point_pos
-    pygame.draw.circle(screen, point_color, (x_point_pos, y_point_pos), radius)
+    def move(self):
+        self.x_pos += self.x_speed_direction * self.square_size
+        self.y_pos += self.y_speed_direction * self.square_size
+        self.x_body.append(self.x_pos)
+        self.y_body.append(self.y_pos)
+        self.x_body.popleft()
+        self.y_body.popleft()
 
-def move_point():
-    global formsize, squareSize, x_point_pos, y_point_pos
-    x_point_pos = (randint(squareSize, formSize - squareSize)//squareSize)*squareSize
-    y_point_pos = (randint(squareSize, formSize - squareSize)//squareSize)*squareSize
+    def set_speed(self, x, y):
+        self.x_speed_direction = x
+        self.y_speed_direction = y
 
+    def get_speed_x(self):
+        return self.x_speed_direction
 
-#form
-formSize = 600
-pygame.init()
-screen = pygame.display.set_mode((formSize, formSize))
-#square
-squareSize = 20
-x_pos = screen.get_rect()[2] / 2 - squareSize / 2 
-y_pos = screen.get_rect()[3] / 2 - squareSize / 2
+    def get_speed_y(self):
+        return self.y_speed_direction
 
-x_body = deque()
-x_body.append(x_pos)
-y_body = deque()
-y_body.append(y_pos)
+    def is_self_intersect(self):
+        for i in range(0, self.counter - 1):  # off with her head
+            if self.x_body[i] == self.x_pos and self.y_body[i] == self.y_pos:
+                return True
+        return False
 
+    def eat_point(self):
+        self.x_body.appendleft(self.x_body[0])
+        self.y_body.appendleft(self.y_body[0])
+        self.counter += 1
 
-#point
-radius = 3
-x_point_pos = (randint(squareSize/2, formSize - squareSize/2)//squareSize)*squareSize
-y_point_pos = (randint(squareSize/2, formSize - squareSize/2)//squareSize)*squareSize
-point_color = (255, 165, 0)
+    def draw(self):
+        for i in range(0, self.counter):
+            pygame.draw.rect(self.screen, (0, 0, 255), pygame.Rect(self.x_body[i], self.y_body[i], self.square_size, self.square_size))
 
-
-time = 30/20
-x_speed_direction = 1
-y_speed_direction = 0
-
-pygame.init()
-screen = pygame.display.set_mode((formSize, formSize))
-
-
-clock = pygame.time.Clock()
-
-done = False
-counter = 1
-eaten = False
-button = False
-
-while not done:
-    
-    screen.fill((0, 0, 0))
-    button = False
-    eaten = False
-        
-    for i in range (0, counter):
-        draw_body(x_body[i], y_body[i])
-
-    if x_point_pos == (x_pos + squareSize/2) and y_point_pos == (y_pos + squareSize/2):            
-        x_body.appendleft(x_body[0])
-        y_body.appendleft(y_body[0])
-        
-        counter += 1
-        if counter % 10 == 0:
-            time = time * 1.2
-        move_point()
-        eaten = True
-        
-    if not eaten:
-        draw_point()
-    
-    for event in pygame.event.get():            
-        if event.type == pygame.QUIT:
-            done = True
-            pygame.quit()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT and not button:
-                if x_speed_direction == 0:
-                    set_speed(1, 0)
-                    button = True
-                    continue
-                else:
-                    continue
-            if event.key == pygame.K_LEFT and not button:
-                if x_speed_direction == 0:
-                    set_speed(-1, 0)
-                    button = True
-                    continue
-                else:
-                    continue
-            if event.key == pygame.K_UP and not button:
-                if y_speed_direction == 0:
-                    set_speed(0, -1)
-                    button = True
-                    continue
-                else:
-                    continue
-            if event.key == pygame.K_DOWN and not button:
-                if y_speed_direction == 0:
-                    set_speed(0, 1)
-                    button = True
-                    continue
-                else:
-                    continue
-    move()
-    pygame.display.flip()
-    clock.tick(time)
-    
